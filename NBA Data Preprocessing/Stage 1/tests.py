77e60cecc09e129b7ed42a8fdb431360ab937226
molotkova <molotkova548@gmail.com>
2022-08-11 19:20:53 +0300

    added extra checks to stage 1

--- NBA Data Preprocessing/Stage 1/tests.py
+++ NBA Data Preprocessing/Stage 1/tests.py
@@ -1,6 +1,7 @@
 from hstest import StageTest, TestCase, CheckResult
 from hstest.stage_test import List
 import pandas as pd
+import os
 
 module = True
 type_err = True
@@ -12,9 +13,11 @@ try:
 except ImportError:
     clean_data = None
     module = False
-except TypeError:
+except TypeError as type_err_exc:
+    type_err_exc_message = type_err_exc
     type_err = False
-except Exception:
+except Exception as other_exc:
+    other_exc_message = other_exc
     other_err = False
 
 
@@ -25,14 +28,22 @@ class Tests(StageTest):
 
     def check(self, reply: str, attach):
 
+        if not os.path.exists('preprocess.py'):
+            return CheckResult.wrong('The file `preprocess.py` is not found. Your solution should be located there.\n'
+                                     'Please do not rename the file.')
+
         if not module:
             return CheckResult.wrong('The function `clean_data` was not found in your solution.')
 
         if not type_err:
-            return CheckResult.wrong("Do not forget the input parameter: path")
+            return CheckResult.wrong(f"An error occurred during execution of your solution.\n"
+                                     f"The function `clean_data` should take one input parameter: path to the dataset.\n"
+                                     f"An internal error message:\n{type_err_exc_message}")
 
         if not other_err:
-            return CheckResult.wrong("An error occurred during execution of `clean_data` function. Refer to the Objectives and Examples sections.")
+            return CheckResult.wrong(f"An error occurred during execution of `clean_data` function.\n"
+                                     f"The error message:\n{other_exc_message}\n\n"
+                                     f"Refer to the Objectives and Examples sections.")
 
         if df is None:
             return CheckResult.wrong('The `clean_data` function returns nothing while it should return a DataFrame')
